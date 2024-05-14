e5bfe0d1d48485598d3dc1ca1f97038cd0f5d0a4
bmribeiro <bruno.miguel.rib@gmail.com>
2024-05-14 23:08:39 +0100

    AttributeOption - @GetMapping (byID) ; @PutMapping ; @DeleteMapping (byID) and @Service(s)

--- javarest/src/main/java/com/onlinestore/javarest/controller/AttributeOptionController.java
+++ javarest/src/main/java/com/onlinestore/javarest/controller/AttributeOptionController.java
@@ -1,12 +1,17 @@
 package com.onlinestore.javarest.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,17 +23,34 @@ import com.onlinestore.javarest.service.AttributeOptionService;
 @RequestMapping("/api/attribute-options")
 public class AttributeOptionController {
 
-    @Autowired
-    private AttributeOptionService attributeOptionService;
+	@Autowired
+	private AttributeOptionService attributeOptionService;
 
-    @GetMapping
-    public List<AttributeOption> getAllAttributeOptions() {
-        return attributeOptionService.getAllAttributeOptions();
-    }
-    
-    @PostMapping
-    public ResponseEntity<AttributeOption> createBrand(@RequestBody AttributeOption attributeOption) {
-    	AttributeOption savedAttributeOption = attributeOptionService.addAttributeOption(attributeOption);
-        return new ResponseEntity<>(savedAttributeOption, HttpStatus.CREATED);
-    }
+	@GetMapping
+	public List<AttributeOption> getAllAttributeOptions() {
+		return attributeOptionService.getAllAttributeOptions();
+	}
+
+	@GetMapping(path = "/{id}")
+	public Optional<AttributeOption> getAttributeOptionsById(@PathVariable Long id) {
+		return attributeOptionService.getAttributeOptionById(id);
+	}
+
+	@PostMapping
+	public ResponseEntity<AttributeOption> createBrand(@RequestBody AttributeOption attributeOption) {
+		AttributeOption savedAttributeOption = attributeOptionService.addAttributeOption(attributeOption);
+		return new ResponseEntity<>(savedAttributeOption, HttpStatus.CREATED);
+	}
+
+	@PutMapping()
+	public AttributeOption updateAttributeOptions(@RequestBody AttributeOption attributeOption) {
+		return attributeOptionService.updateAttributeOption(attributeOption);
+	}
+
+	@DeleteMapping(path = "/{id}")
+	public ResponseEntity<Void> deleteAttributeOptionsById(@PathVariable Long id) {
+		attributeOptionService.deleteAttributeOptionById(id);
+		return ResponseEntity.ok().build();
+	}
+
 }
--- javarest/src/main/java/com/onlinestore/javarest/service/AttributeOptionService.java
+++ javarest/src/main/java/com/onlinestore/javarest/service/AttributeOptionService.java
@@ -1,12 +1,15 @@
 package com.onlinestore.javarest.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.onlinestore.javarest.entities.AttributeOption;
 import com.onlinestore.javarest.repository.AttributeOptionRepository;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class AttributeOptionService {
@@ -18,7 +21,20 @@ public class AttributeOptionService {
 		return (List<AttributeOption>) attributeOptionRepository.findAll();
 	}
 
+	public Optional<AttributeOption> getAttributeOptionById(Long attributeOptionId) {
+		return attributeOptionRepository.findById(attributeOptionId.intValue());
+	}
+
 	public AttributeOption addAttributeOption(AttributeOption attributeOptionEntity) {
 		return attributeOptionRepository.save(attributeOptionEntity);
+	}
+
+	public AttributeOption updateAttributeOption(AttributeOption attributeOptionEntity) {
+		return attributeOptionRepository.save(attributeOptionEntity);
+	}
+
+	@Transactional
+	public void deleteAttributeOptionById(Long attributeOptionId) {
+		attributeOptionRepository.deleteById(attributeOptionId.intValue());
 	}
 }
